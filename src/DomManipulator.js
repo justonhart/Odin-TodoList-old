6412b7151a312c8e7a6d6ad76e4f55d68beffb8c
justonhart <contact@justonh.art>
2020-08-13 18:01:08 -0500

    Add functionality to projects, todos, and details

--- src/DomManipulator.js
+++ src/DomManipulator.js
@@ -1,21 +1,144 @@
-const renderProjects = (projects) => {
+const renderProject = (project) => {
+    let projectDiv = document.createElement("div");
+    projectDiv.setAttribute("class", "project");
+    projectDiv.innerText = project.name;
 
+    let button = document.getElementById('newProject');
+    document.getElementById('projects').insertBefore(projectDiv, button);
+    return projectDiv;
 };
 
-const renderTodos = (todos) => {
+const renderTodo = (todo) => {
+    
+    const todosPane = document.getElementById("todos");
+    
+    let todoDiv = document.createElement("div");
+    todoDiv.setAttribute("class", "todo");
+    todoDiv.innerText = todo.name;
 
+    let button = document.getElementById('newTodo');
+    todosPane.insertBefore(todoDiv, button);
+    return todoDiv;
 };
 
-const switchDetailsToEdit = () => {
+const clearProjects = () => {
+    let projectsContainer = document.getElementById('projects');
 
-};
+    while(projectsContainer.childNodes.length > 2){
+        projectsContainer.removeChild(projectsContainer.firstChild);
+    }
+}
 
-const switchDetailsToView = () => {
+const clearTodos = () => {
+    let todosContainer = document.getElementById('todos');
+
+    while(todosContainer.childNodes.length > 2){
+        todosContainer.removeChild(todosContainer.firstChild);
+    }
+}
+
+const switchDetailsToEdit = (todo, saveChanges) => {
     const details = document.getElementById("details");
 
     while(details.lastChild){
         details.removeChild(details.lastChild);
     }
+
+    details.innerHTML = `
+        Details Pane
+        <div class="detailsDiv">
+            Title: <input type="text" value="${todo.name}" id="titleInput">
+        </div>
+        <div class="detailsDiv">
+            Description: <input type="text" value="${todo.details}" id="detailsInput" name=details>
+        </div>
+        <div class="detailsDiv">
+            Due Date: <input type="date" id="dueDateSelector" value ="${todo.dueDate}">
+        </div>
+    `;
+    
+    let selectDiv = document.createElement("div");
+    selectDiv.setAttribute("class", "detailsDiv");
+    switch(todo.priority){
+        case "Low":
+            selectDiv.innerHTML = `
+            <div class="detailsDiv">
+            Priority: <select id="prioritySelector">
+                <option value="Low" selected>Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High">High</option>bra
+            </select>
+            </div>`;
+            break;
+        case "Medium":
+            selectDiv.innerHTML = `
+            <div class="detailsDiv">
+            Priority: <select id="prioritySelector">
+                <option value="Low">Low</option>
+                <option value="Medium" selected>Medium</option>
+                <option value="High">High</option>bra
+            </select>
+            </div>`;
+            break;
+        case "High":
+            selectDiv.innerHTML = `
+            <div class="detailsDiv">
+            Priority: <select id="prioritySelector">
+                <option value="Low">Low</option>
+                <option value="Medium">Medium</option>
+                <option value="High" selected>High</option>bra
+            </select>
+            </div>`;
+            break;
+    }
+
+    details.appendChild(selectDiv);
+
+    let saveButton = document.createElement("button");
+    saveButton.innerText = "Save";
+    saveButton.addEventListener("click", function(){
+        saveChanges({
+            name: document.getElementById("titleInput").value,
+            details: document.getElementById("detailsInput").value,
+            dueDate: document.getElementById("dueDateSelector").value,
+            priority: document.getElementById("prioritySelector").value
+        });
+    });
+    details.appendChild(saveButton);
+
 };
 
-export {switchDetailsToView};
+const switchDetailsToView = (todo, editFunction) => {
+    const details = document.getElementById("details");
+
+    while(details.lastChild){
+        details.removeChild(details.lastChild);
+    }
+
+    details.innerHTML = `
+        Details Pane
+        <div class="detailsDiv">
+            Title: ${todo.name}
+        </div>
+        <div class="detailsDiv">
+            Description: ${todo.details}
+        </div>
+        <div class="detailsDiv">
+            Due Date: ${todo.dueDate}
+        </div>
+        <div class="detailsDiv">
+            Priority: ${todo.priority}
+            </select>
+        </div>
+        <button id="editButton">Edit</button>
+    `;
+    document.getElementById("editButton").addEventListener("click", editFunction);
+};
+
+const clearDetails = () => {
+    let detailsContainer = document.getElementById('details');
+
+    detailsContainer.innerHTML = null;
+};
+
+export {renderProject, renderTodo, switchDetailsToView, switchDetailsToEdit, clearProjects, clearTodos, clearDetails};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
-import {switchDetailsToView} from "./DomManipulator.js";
+import {renderProject, renderTodo, switchDetailsToView, switchDetailsToEdit, clearProjects, clearTodos, clearDetails} from "./DomManipulator.js";
+import {createTodo} from "./factory"
+
+//load data from localStorage
+
+let projects = [];
+let selectedProject, selectedTodo;
+projects = getDefaultProjects();
+
+
+// Render projects and todos
+// this should add the listeners to those objects
+displayProjects();
+
+
+
+function getDefaultProjects(){
+    let defaultProjects = [];
+    let nonemptyProject = {name: "Uncategorized", todos: []};
+    nonemptyProject.todos.push(createTodo("Wash dog", undefined, undefined, "Low"));
+    nonemptyProject.todos.push(createTodo("Trash", undefined, undefined, "Medium"));
+    nonemptyProject.todos.push(createTodo("Lorem", undefined, undefined, "High"));
+
+    defaultProjects.push(nonemptyProject);
+    defaultProjects.push({name: "Empty", todos: []});
+    return defaultProjects;
+}
+
+//provided to the switch details to view function to provide functionality to the "Edit" button it produces
+function switchToEditing(){
+    switchDetailsToEdit(projects[selectedProject].todos[selectedTodo], saveChangesFromDetails);
+}
+
+//redraws projects container and assigns necessary listeners
+function displayProjects(){
+    clearProjects();
+    projects.forEach((p, index) => {
+        let pDiv = renderProject(p);
+        pDiv.addEventListener("click", function(){
+            if(selectedProject != index){
+                selectedProject = index;
+                clearDetails();
+                selectedTodo = undefined;
+                displayTodos();
+            }
+            
+        })
+    });
+}
+
+//redraws todos container and assigns necessary listeners
+function displayTodos(){
+    clearTodos();
+    projects[selectedProject].todos.forEach((t, index) => {
+        let todoDiv = renderTodo(t);
+        todoDiv.addEventListener("click", function(){
+            if(selectedTodo != index){
+                selectedTodo = index;
+                switchDetailsToView(projects[selectedProject].todos[selectedTodo], switchToEditing);
+            }
+        })
+    });
+}
+
+//this function is passed to the DomManipulator so the "Save" button in the details pane has soemthing to do
+function saveChangesFromDetails(todo){
+    projects[selectedProject].todos[selectedTodo] = todo;
+    switchDetailsToView(projects[selectedProject].todos[selectedTodo], switchToEditing);
+    displayTodos();
+    
+}
